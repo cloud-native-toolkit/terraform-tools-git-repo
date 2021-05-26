d4cc835ef726163bc0528388af663b241d63e537
Sean Sundberg <seansund@us.ibm.com>
2021-05-26 00:42:08 -0500

    Fixes triggers
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- main.tf
+++ main.tf
@@ -3,11 +3,14 @@ resource null_resource create_gitlab_repo {
   count = var.provision && var.type == "gitlab" ? 1 : 0
 
   triggers = {
+    HOST  = var.host
+    ORG   = var.org
+    REPO  = var.repo
     TOKEN = var.token
   }
 
   provisioner "local-exec" {
-    command = "${path.module}/scripts/create-gitlab-repo.sh ${var.host} ${var.org} ${var.repo}"
+    command = "${path.module}/scripts/create-gitlab-repo.sh ${self.triggers.HOST} ${self.triggers.ORG} ${self.triggers.REPO} ${var.public}"
 
     environment = {
       TOKEN = self.triggers.TOKEN
@@ -16,7 +19,7 @@ resource null_resource create_gitlab_repo {
 
   provisioner "local-exec" {
     when = destroy
-    command = "${path.module}/scripts/delete-gitlab-repo.sh ${var.host} ${var.org} ${var.repo}"
+    command = "${path.module}/scripts/delete-gitlab-repo.sh ${self.triggers.HOST} ${self.triggers.ORG} ${self.triggers.REPO}"
 
     environment = {
       TOKEN = self.triggers.TOKEN
@@ -28,20 +31,23 @@ resource null_resource create_github_repo {
   count = var.provision && var.type == "github" ? 1 : 0
 
   triggers = {
+    HOST  = var.host
+    ORG   = var.org
+    REPO  = var.repo
     TOKEN = var.token
   }
 
   provisioner "local-exec" {
-    command = "${path.module}/scripts/create-github-repo.sh ${var.host} ${var.org} ${var.repo}"
+    command = "${path.module}/scripts/create-github-repo.sh ${self.triggers.HOST} ${self.triggers.ORG} ${self.triggers.REPO} ${var.public}"
 
     environment = {
-      TOKEN = var.token
+      TOKEN = self.triggers.TOKEN
     }
   }
 
   provisioner "local-exec" {
     when = destroy
-    command = "${path.module}/scripts/delete-github-repo.sh ${var.host} ${var.org} ${var.repo}"
+    command = "${path.module}/scripts/delete-github-repo.sh ${self.triggers.HOST} ${self.triggers.ORG} ${self.triggers.REPO}}"
 
     environment = {
       TOKEN = self.triggers.TOKEN
