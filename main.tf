c5b97f6b99ba0132bd67c1cb984c1e97d837c9a5
Sean Sundberg <seansund@us.ibm.com>
2021-06-09 07:21:05 -0500

    Collapses terraform resources and fixes typo in destroy
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 
-resource null_resource create_gitlab_repo {
-  count = var.provision && var.type == "gitlab" ? 1 : 0
+resource null_resource create_repo {
+  count = var.provision ? 1 : 0
 
   triggers = {
+    TYPE  = var.type
     HOST  = var.host
     ORG   = var.org
     REPO  = var.repo
@@ -10,7 +11,7 @@ resource null_resource create_gitlab_repo {
   }
 
   provisioner "local-exec" {
-    command = "${path.module}/scripts/create-gitlab-repo.sh ${self.triggers.HOST} ${self.triggers.ORG} ${self.triggers.REPO} ${var.public}"
+    command = "${path.module}/scripts/create-repo.sh '${self.triggers.TYPE}' '${self.triggers.HOST}' '${self.triggers.ORG}' '${self.triggers.REPO}' '${var.public}'"
 
     environment = {
       TOKEN = self.triggers.TOKEN
@@ -19,35 +20,7 @@ resource null_resource create_gitlab_repo {
 
   provisioner "local-exec" {
     when = destroy
-    command = "${path.module}/scripts/delete-gitlab-repo.sh ${self.triggers.HOST} ${self.triggers.ORG} ${self.triggers.REPO}"
-
-    environment = {
-      TOKEN = self.triggers.TOKEN
-    }
-  }
-}
-
-resource null_resource create_github_repo {
-  count = var.provision && var.type == "github" ? 1 : 0
-
-  triggers = {
-    HOST  = var.host
-    ORG   = var.org
-    REPO  = var.repo
-    TOKEN = var.token
-  }
-
-  provisioner "local-exec" {
-    command = "${path.module}/scripts/create-github-repo.sh ${self.triggers.HOST} ${self.triggers.ORG} ${self.triggers.REPO} ${var.public}"
-
-    environment = {
-      TOKEN = self.triggers.TOKEN
-    }
-  }
-
-  provisioner "local-exec" {
-    when = destroy
-    command = "${path.module}/scripts/delete-github-repo.sh ${self.triggers.HOST} ${self.triggers.ORG} ${self.triggers.REPO}}"
+    command = "${path.module}/scripts/delete-repo.sh '${self.triggers.TYPE}' '${self.triggers.HOST}' '${self.triggers.ORG}' '${self.triggers.REPO}'"
 
     environment = {
       TOKEN = self.triggers.TOKEN
---
+++ scripts/delete-repo.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -e
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd -P)
+
+TYPE="$1"
+HOSTNAME="$2"
+ORG="$3"
+REPO="$4"
+
+if [[ -z "${TYPE}" ]] || [[ -z "${HOSTNAME}" ]] || [[ -z "${ORG}" ]] || [[ -z "${REPO}" ]]; then
+  echo "Usage: delete-repo.sh TYPE HOSTNAME ORG REPO"
+  exit 1
+fi
+
+if [[ -z "${TOKEN}" ]]; then
+  echo "TOKEN environment variable must be set"
+  exit 1
+fi
+
+if [[ -z "${USER}" ]]; then
+  echo "USER environment variable must be set"
+  exit 1
+fi
+
+if [[ "${TYPE}" == "github" ]]; then
+  TOKEN="${TOKEN}" USER="${USER}" "${SCRIPT_DIR}/delete-github-repo.sh" "${HOSTNAME}" "${ORG}" "${REPO}"
+elif [[ "${TYPE}" == "gitlab" ]]; then
+  TOKEN="${TOKEN}" USER="${USER}" "${SCRIPT_DIR}/delete-gitlab-repo.sh" "${HOSTNAME}" "${ORG}" "${REPO}"
+else
+  echo "Unsupported repo type: $TYPE"
+  exit 1
+fi
